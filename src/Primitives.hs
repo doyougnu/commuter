ac809536782c8833855950ca5e518a21f569011b
doyougnu <jmy6342@gmail.com>
2018-04-26 08:51:06 -0700

    adding composition

--- src/Primitives.hs
+++ src/Primitives.hs
@@ -110,18 +110,31 @@ addT (from, lbl, to) mp
   | from `member` mp = adjust ((:) (lbl, to)) from mp
   | otherwise = insert from [(lbl, to)] mp
 
+-- | Given a plain edge and a graph, add that edge to the graph
 pEdge :: (Ord n, Ord l) => Edge n l -> Graph n m l -> Graph n m l
 pEdge e = G . first (addT e) . unG
 
-addHEdge :: (Ord l) => Edge l m -> Graph n m l -> Graph n m l
-addHEdge e = G . second (addT e) . unG
+-- | a hyper edge is from a edge to and edge
+hEdge :: (Ord l) => Edge l m -> Graph n m l -> Graph n m l
+hEdge e = G . second (addT e) . unG
 
+-- | Compose to edges
+o :: Monoid l => Edge n l -> Edge n l -> Edge n l -- this is monoidic
+o (_, ll, d) (a, l, _) = (a, ll <> l, d)
+
+o' :: Show l => Edge n l -> Edge n l -> Edge n String
+o' (_, ll, d) (a, l, _) = (a, show ll ++ " o " ++ show l, d)
+
+
+-- | A node is just a singleton graph with no edges
 node :: (Ord n, Ord l) => n -> Graph n m l
 node k = G (singleton k [], empty)
 
+-- | An id edge is a edge from a node to itself with the label id
 _idEdge :: (IsString l) => n -> Edge n l
 _idEdge n = (n, "id", n)
 
+-- | Given a node, and a graph, create a id edge and add it to the graph
 idEdge :: (Ord n, Ord m, Ord l, IsString l) => n -> Graph n m l -> Graph n m l
 idEdge = pEdge . _idEdge
 
