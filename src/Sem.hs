316f375cdaf880e93da904dc16a9e9077417bb5b
doyougnu <jmy6342@gmail.com>
2018-05-26 09:09:41 -0700

    adding lenses and cairo backend

--- src/Sem.hs
+++ src/Sem.hs
@@ -1,6 +1,6 @@
 module Sem where
 
-import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.Prelude hiding (from, to, fc, (<>), adjust, trace)
 
 import Data.Typeable (Typeable)
